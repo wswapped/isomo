9fbc3c0650a122b07f64e846821a3c32f3e92208
wswapped <wswapped@gmail.com>
2018-03-29 07:42:20 +0200

    tinyMCE

--- admin/add_paper.php
+++ admin/add_paper.php
@@ -5,9 +5,9 @@
 <html lang="en">
 <head>
   <?php
-  $title = "Add paper";
-  include_once 'modules/head.php';
-  $languages = array('kin'=>'Ikinyarwanda', 'en'=>'English', 'fr'=>'France');
+    $title = "Add paper";
+    include_once 'modules/head.php';
+    $languages = array('kin'=>'Ikinyarwanda', 'en'=>'English', 'fr'=>'France');
   ?>
 
   <!-- plugins -->
@@ -15,6 +15,7 @@
   <link rel="stylesheet" type="text/css" href="asset/css/plugins/animate.min.css"/>
   <link href="asset/css/style.css" rel="stylesheet">
   <link rel="stylesheet" type="text/css" href="asset/css/plugins/select2.min.css"/>
+  <link rel="stylesheet" type="text/css" href="asset/css/plugins/summernote.css"/>
   <!-- end: Css -->
 </head>
 
@@ -61,11 +62,17 @@
                     ?>
                     <div class="panel-heading"><h3><?php echo $paper_cat_name; ?></h3></div>
                     <div class="panel-body">
-
-                      <form class="cmxform" id="signupForm" method="get" action="" novalidate="novalidate">
+                      <form class="cmxform" id="signupForm" method="POST" action="<?php echo $_SERVER['REQUEST_URI'] ?>" novalidate="novalidate">
                           <div class="col-md-6">
 
                             <?php
+
+
+                            //checking form submission
+                            if(!empty($_POST['subt'])){
+                              var_dump($_POST);
+                            }
+
                               //subject types and subject
                               $query = $db->query("SELECT *, s.name as subjectname FROM subjects as s JOIN subject_levels as l ON s.id  = l.subject ") or die("Error $db->error");
                               $subj = array();
@@ -76,14 +83,14 @@
 
 
                             <div class="form-group">
-                              <select class="select2-A">
+                              <select class="select2-A" name="subject">
                                 <?php
                                   foreach ($subj as $key => $subjects ) {
                                     ?>
                                       <optgroup label="<?php echo $key; ?>">
                                         <?php
                                           foreach ($subjects as $key => $subj_det) {
-                                            echo "<option value=''>$subj_det[subjectname]</option>";
+                                            echo "<option value='$subj_det[id]'>$subj_det[subjectname]</option>";
                                           }
                                         ?>
                                       </optgroup>
@@ -91,19 +98,26 @@
                                   }
                                 ?>
                               </select>
+                              <input type="hidden" name="subt" value="<?php echo md5(time()) ?>">
                             </div>
                             <div class="form-group form-cond" data-for="national_exams" data-role='year'>
                               <label for="fileup">Paper year</label>
-                              <input type="number" min="1900" max="<?php echo date('Y'); ?>" name="year" class="form-control" id="fileup" placeholder="year">
+                              <input type="number" min="1900" max="<?php echo date('Y'); ?>" name="year" class="form-control" id="fileup" placeholder="year" required="required">
                             </div>
-                          </div>                 
+                            <div class="form-group">
+	                          	<textarea name="paper" id="paperContEdit" placeholder="Type Your Message.." required="required"></textarea>  
+	                          </div>
+                          </div>
+                          
+                               
                           <div class="col-md-12">
-                              <div class="form-group form-animate-checkbox">
-                                  <input type="checkbox" class="checkbox valid" id="validate_agree" name="validate_agree" aria-required="true" aria-describedby="validate_agree-error">
-                                  <label>Please agree to our policy</label>
-                              <em id="validate_agree-error" class="error"></em></div>
-                              <input class="submit btn btn-danger" type="submit" value="Submit">
-                        </div>
+                            <div class="form-group form-animate-checkbox">
+                                <input type="checkbox" class="checkbox valid" id="validate_agree" name="validate_agree" aria-required="true" aria-describedby="validate_agree-error">
+                                <label>Please agree to our policy</label>
+                            	<em id="validate_agree-error" class="error"></em>
+                          	</div>
+                            <input class="submit btn btn-danger" type="submit" value="Submit">
+                          </div>
                       </form>
 
                     </div>
@@ -236,140 +250,6 @@
       </div>
 
       <!-- start: Mobile -->
-      <div id="mimin-mobile" class="reverse">
-        <div class="mimin-mobile-menu-list">
-            <div class="col-md-12 sub-mimin-mobile-menu-list animated fadeInLeft">
-                <ul class="nav nav-list">
-                    <li class="active ripple">
-                      <a class="tree-toggle nav-header">
-                        <span class="fa-home fa"></span>Dashboard 
-                        <span class="fa-angle-right fa right-arrow text-right"></span>
-                      </a>
-                      <ul class="nav nav-list tree">
-                          <li><a href="dashboard-v1.html">Dashboard v.1</a></li>
-                          <li><a href="dashboard-v2.html">Dashboard v.2</a></li>
-                      </ul>
-                    </li>
-                    <li class="ripple">
-                      <a class="tree-toggle nav-header">
-                        <span class="fa-diamond fa"></span>Layout
-                        <span class="fa-angle-right fa right-arrow text-right"></span>
-                      </a>
-                      <ul class="nav nav-list tree">
-                        <li><a href="topnav.html">Top Navigation</a></li>
-                        <li><a href="boxed.html">Boxed</a></li>
-                      </ul>
-                    </li>
-                    <li class="ripple">
-                      <a class="tree-toggle nav-header">
-                        <span class="fa-area-chart fa"></span>Charts
-                        <span class="fa-angle-right fa right-arrow text-right"></span>
-                      </a>
-                      <ul class="nav nav-list tree">
-                        <li><a href="chartjs.html">ChartJs</a></li>
-                        <li><a href="morris.html">Morris</a></li>
-                        <li><a href="flot.html">Flot</a></li>
-                        <li><a href="sparkline.html">SparkLine</a></li>
-                      </ul>
-                    </li>
-                    <li class="ripple">
-                      <a class="tree-toggle nav-header">
-                        <span class="fa fa-pencil-square"></span>Ui Elements
-                        <span class="fa-angle-right fa right-arrow text-right"></span>
-                      </a>
-                      <ul class="nav nav-list tree">
-                        <li><a href="color.html">Color</a></li>
-                        <li><a href="weather.html">Weather</a></li>
-                        <li><a href="typography.html">Typography</a></li>
-                        <li><a href="icons.html">Icons</a></li>
-                        <li><a href="buttons.html">Buttons</a></li>
-                        <li><a href="media.html">Media</a></li>
-                        <li><a href="panels.html">Panels & Tabs</a></li>
-                        <li><a href="notifications.html">Notifications & Tooltip</a></li>
-                        <li><a href="badges.html">Badges & Label</a></li>
-                        <li><a href="progress.html">Progress</a></li>
-                        <li><a href="sliders.html">Sliders</a></li>
-                        <li><a href="timeline.html">Timeline</a></li>
-                        <li><a href="modal.html">Modals</a></li>
-                      </ul>
-                    </li>
-                    <li class="ripple">
-                      <a class="tree-toggle nav-header">
-                       <span class="fa fa-check-square-o"></span>Forms
-                       <span class="fa-angle-right fa right-arrow text-right"></span>
-                      </a>
-                      <ul class="nav nav-list tree">
-                        <li><a href="formelement.html">Form Element</a></li>
-                        <li><a href="#">Wizard</a></li>
-                        <li><a href="#">File Upload</a></li>
-                        <li><a href="#">Text Editor</a></li>
-                      </ul>
-                    </li>
-                    <li class="ripple">
-                      <a class="tree-toggle nav-header">
-                        <span class="fa fa-table"></span>Tables
-                        <span class="fa-angle-right fa right-arrow text-right"></span>
-                      </a>
-                      <ul class="nav nav-list tree">
-                        <li><a href="datatables.html">Data Tables</a></li>
-                        <li><a href="handsontable.html">handsontable</a></li>
-                        <li><a href="tablestatic.html">Static</a></li>
-                      </ul>
-                    </li>
-                    <li class="ripple">
-                      <a href="calendar.html">
-                         <span class="fa fa-calendar-o"></span>Calendar
-                      </a>
-                    </li>
-                    <li class="ripple">
-                      <a class="tree-toggle nav-header">
-                        <span class="fa fa-envelope-o"></span>Mail
-                        <span class="fa-angle-right fa right-arrow text-right"></span>
-                      </a>
-                      <ul class="nav nav-list tree">
-                        <li><a href="mail-box.html">Inbox</a></li>
-                        <li><a href="compose-mail.html">Compose Mail</a></li>
-                        <li><a href="view-mail.html">View Mail</a></li>
-                      </ul>
-                    </li>
-                    <li class="ripple">
-                      <a class="tree-toggle nav-header">
-                        <span class="fa fa-file-code-o"></span>Pages
-                        <span class="fa-angle-right fa right-arrow text-right"></span>
-                      </a>
-                      <ul class="nav nav-list tree">
-                        <li><a href="forgotpass.html">Forgot Password</a></li>
-                        <li><a href="login.html">SignIn</a></li>
-                        <li><a href="reg.html">SignUp</a></li>
-                        <li><a href="article-v1.html">Article v1</a></li>
-                        <li><a href="search-v1.html">Search Result v1</a></li>
-                        <li><a href="productgrid.html">Product Grid</a></li>
-                        <li><a href="profile-v1.html">Profile v1</a></li>
-                        <li><a href="invoice-v1.html">Invoice v1</a></li>
-                      </ul>
-                    </li>
-                     <li class="ripple"><a class="tree-toggle nav-header"><span class="fa "></span> MultiLevel  <span class="fa-angle-right fa right-arrow text-right"></span> </a>
-                      <ul class="nav nav-list tree">
-                        <li><a href="view-mail.html">Level 1</a></li>
-                        <li><a href="view-mail.html">Level 1</a></li>
-                        <li class="ripple">
-                          <a class="sub-tree-toggle nav-header">
-                            <span class="fa fa-envelope-o"></span> Level 1
-                            <span class="fa-angle-right fa right-arrow text-right"></span>
-                          </a>
-                          <ul class="nav nav-list sub-tree">
-                            <li><a href="mail-box.html">Level 2</a></li>
-                            <li><a href="compose-mail.html">Level 2</a></li>
-                            <li><a href="view-mail.html">Level 2</a></li>
-                          </ul>
-                        </li>
-                      </ul>
-                    </li>
-                    <li><a href="credits.html">Credits</a></li>
-                  </ul>
-            </div>
-        </div>       
-      </div>
       <button id="mimin-mobile-menu-opener" class="animated rubberBand btn btn-circle btn-danger">
         <span class="fa fa-bars"></span>
       </button>
@@ -389,7 +269,10 @@
 <script src="asset/js/plugins/datatables.bootstrap.min.js"></script>
 <script src="asset/js/plugins/jquery.nicescroll.js"></script>
 <script src="asset/js/plugins/select2.full.min.js"></script>
+<script src="asset/js/plugins/summernote.min.js"></script>
 
+<script src="asset/js/plugins/tinymce/tinymce.min.js"></script>
+<script type="text/javascript" src="js/"></script>
 
 <!-- custom -->
 <script type="text/javascript" src="asset/js/js.js"></script>
@@ -400,9 +283,20 @@
   });
 
   $(".select2-A").select2({
-      placeholder: "Select a subject",
-      allowClear: true
-    });
+    placeholder: "Select a subject",
+    allowClear: true
+  });
+
+  // tinymce.init({
+  //   selector: '#paperContEdit',
+  //   menubar: false,  // removes the menubar
+  //   image_advtab: true,
+  // });
+
+  $('#paperContEdit').summernote({
+    height: 400
+  });
+
 </script>
 <!-- end: Javascript -->
 </body>
