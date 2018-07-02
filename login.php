7206f497ee00a6ea1ebfcad66dc36991cf45e47a
wswapped <wswapped@gmail.com>
2018-07-02 22:21:06 +0200

    Adapt login

--- login.php
+++ login.php
@@ -25,72 +25,64 @@
 </head>
 <body>
 	<?php include "modules/menu.php"; ?>
-	<div class="container">
-		<div class="col-sm-12">
-			<div class="card" style="max-width: 650px; margin: 0 auto">
-				<div class="card-block">
-					<h1 class="card-title page-title">Log in</h1>
-					<div class="row">
-							<div class="col-md-12">
-								<form method="POST" action="login">
-									<?php
-										if($_SERVER['REQUEST_METHOD'] == 'POST'){
-											$username = $_POST['username']??"";
-											$password = $_POST['password']??"";
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-sm-12">
+				<div class="card mt-5" style="max-width: 650px; margin: 0 auto">
+					<div class="card-body">
+						<h1 class="card-title page-title">Log in</h1>
+						<form method="POST" action="login">
+							<?php
+								if($_SERVER['REQUEST_METHOD'] == 'POST'){
+									$username = $_POST['username']??"";
+									$password = $_POST['password']??"";
 
 
-											if($username && $password){
-												//check the login
-												$userId = $User->login($username, $password);
-												if($userId){
+									if($username && $password){
+										//check the login
+										$userId = $User->login($username, $password);
+										if($userId){
 
-													//checking if there is a redirect link set
-													$red_link = $_SESSION['enterlink']??"index";
+											//checking if there is a redirect link set
+											$red_link = $_SESSION['enterlink']??"index";
 
 
-													//unset the link
-													unset($_SESSION['enterlink']);
+											//unset the link
+											unset($_SESSION['enterlink']);
 
-													header("location:$red_link");
-												}else{
-													echo '<p class="text-warning">Invalid username or password</p>';
-												}
-											}else{
-												echo '<p class="text-warning">Enter all the required inputs</p>';
-											}
+											header("location:$red_link");
+										}else{
+											echo '<p class="text-warning">Invalid username or password</p>';
 										}
-										//checking if there is a message set for login information
-										if($login_message = $_SESSION['loginmessage']){
-											echo '<div class="alert alert-success" role="alert">'.$login_message.'
-													</div>';	
-										}
-										
-
-									?>
-								  <div class="form-group">
-									<label for="usernameInput">Username</label>
-									<input type="text" class="form-control text-in" id="usernameInput" name="username" aria-describedby="emailHelp" placeholder="Enter your username" <?php $input = retain_input('post', 'username'); echo $input!=false?'value="'.$input.'"':''; ?> required>
-								  </div>
-								  
-								  <div class="form-group">
-									<label for="inputPassword1">Password</label>
-									<input type="password" class="form-control text-in" id="inputPassword1" name="password" placeholder="Password" required>
-								  </div>
-								  <button type="submit" class="btn btn-primary">Login</button>
-								 	
-								</form>
-								<div class="mt-5">
-							 		<p class="text-muted">New to Isomo?</p>
-							  		<a href="signup">Sign up</a>
-							 	</div>
+									}else{
+										echo '<p class="text-warning">Enter all the required inputs</p>';
+									}
+								}
+								//checking if there is a message set for login information
+								if($login_message = $_SESSION['loginmessage']){
+									echo '<div class="alert alert-success" role="alert">'.$login_message.'
+											</div>';	
+								}
+							?>
+							<div class="form-group">
+								<label for="usernameInput">Username</label>
+								<input type="text" class="form-control text-in" id="usernameInput" name="username" aria-describedby="emailHelp" placeholder="Enter your username" <?php $input = retain_input('post', 'username'); echo $input!=false?'value="'.$input.'"':''; ?> required>
 							</div>
+							  
+							<div class="form-group">
+								<label for="inputPassword1">Password</label>
+								<input type="password" class="form-control text-in" id="inputPassword1" name="password" placeholder="Password" required>
+							</div>
+							<button type="submit" class="btn btn-primary">Login</button>
+						</form>
+						<div class="mt-5">
+					 		<p class="text-muted">New to Isomo?</p>
+					  		<a href="signup">Sign up</a>
+					 	</div>
 					</div>
 				</div>
 			</div>
 		</div>
-	</div>
-	<div class="container">
-		
 	</div>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 
